103ea89d4455eff831259fb590ca7f4877f9be54
Joshua Crunden <joshuacrunden@gmail.com>
2021-08-14 09:34:26 +0100

    Fix linting issues #3

--- static/js/headerAndFooter.js
+++ static/js/headerAndFooter.js
@@ -1,47 +1,47 @@
-const hamburger = document.querySelector('.hamburger');
-const cross = document.querySelector('.cross');
-const menu = document.querySelector('.menu');
-const menuItem = document.querySelectorAll('.menu-item')
-const menuLink = document.querySelectorAll('.menu-link');
+const hamburger = document.querySelector(".hamburger");
+const cross = document.querySelector(".cross");
+const menu = document.querySelector(".menu");
+const menuItem = document.querySelectorAll(".menu-item");
+const menuLink = document.querySelectorAll(".menu-link");
 const mq = window.matchMedia( "(min-width: 970px)" );
-const subItems = document.querySelectorAll('.sub-items');
-const subMenuNames = document.querySelectorAll('.sub-menu-name');
-const chevrons = document.querySelectorAll('.sub-menu-name .fa-chevron-down');
+const subItems = document.querySelectorAll(".sub-items");
+const subMenuNames = document.querySelectorAll(".sub-menu-name");
+const chevrons = document.querySelectorAll(".sub-menu-name .fa-chevron-down");
 
 const flattenNavItems = () => {
-    menu.classList.add('flatten');
-    menu.style.height = '0px';
+    menu.classList.add("flatten");
+    menu.style.height = "0px";
 	closeAllSubMenuForMobile();
-}
+};
 
 const setNavBarHeight = () => {
     const navBarHeight = menuItem[0].scrollHeight * menuItem.length;
-    menu.style.height = navBarHeight + 'px';
-}
+    menu.style.height = navBarHeight + "px";
+};
 
 const openNavItems = () => {
-    menu.classList.remove('flatten');
+    menu.classList.remove("flatten");
     setNavBarHeight();
-}
+};
 
 const openHamburger = () => {
     hamburger.classList.add("hidden", "no-display");
     cross.classList.remove("hidden", "no-display");
     openNavItems();
-}
+};
 
 const closeHamburger = () => {
 	closeAllSubMenuForMobile();
     cross.classList.add("hidden", "no-display");
     hamburger.classList.remove("hidden", "no-display");
     flattenNavItems();
-}
+};
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener("click", () => {
     openHamburger();
 });
 
-cross.addEventListener('click', () => {
+cross.addEventListener("click", () => {
     closeHamburger();
 });
 
@@ -53,33 +53,33 @@ const openCloseSubMenuOnHover = (e) => {
 		subPages.classList.remove("hidden");
 	else
 		subPages.classList.add("hidden");
-}
+};
 
 const closeAllSubMenuForMobile = () => {
 	subItems.forEach(e => {
 	const subMenuHeight = Number.parseInt(e.style.height);
 	if(subMenuHeight > 0)
-		menu.style.height = Number.parseInt(menu.style.height) - subMenuHeight + 'px';
+		menu.style.height = Number.parseInt(menu.style.height) - subMenuHeight + "px";
 	e.classList.add("hidden");
-	e.classList.add('flatten');
-	e.style.height = '0px';
+	e.classList.add("flatten");
+	e.style.height = "0px";
 	});
 	chevrons.forEach(e => e.classList.remove("turn-chevron"));
-}
+};
 
 const openSubMenu = e => {
 	e.classList.remove("hidden");
 	e.classList.remove("flatten");
 	const subMenuHeight = setSubMenuHeight(e);
-	menu.style.height = Number.parseInt(menu.style.height) + subMenuHeight + 'px';
-}
+	menu.style.height = Number.parseInt(menu.style.height) + subMenuHeight + "px";
+};
 
 const setSubMenuHeight = (e) => {
-	const items = e.querySelectorAll('a');
-  	const height = items[0].scrollHeight * items.length;
-  	e.style.height = height + 'px';
+	const items = e.querySelectorAll("a");
+    const height = items[0].scrollHeight * items.length;
+    e.style.height = height + "px";
 	return height;
-}
+};
 
 const openCloseSubMenuForMobile = (e) => {
 	const sb = e.currentTarget.nextElementSibling;
@@ -87,41 +87,41 @@ const openCloseSubMenuForMobile = (e) => {
 	closeAllSubMenuForMobile();
 	if(isClosed) {
 		openSubMenu(sb);
-		e.currentTarget.querySelector('.fa-chevron-down').classList.toggle("turn-chevron");
+		e.currentTarget.querySelector(".fa-chevron-down").classList.toggle("turn-chevron");
 	}
-}
+};
 
 const widthChange = (mq) => {
     if (mq.matches) {
         menu.classList.remove("flatten");
-        menu.style.height = 'unset';
+        menu.style.height = "unset";
         hamburger.classList.add("hidden","no-display");
         cross.classList.add("hidden", "no-display");
-        menu.classList.remove('collapsible');
-        menuLink.forEach(item => item.removeEventListener('click', closeHamburger));
+        menu.classList.remove("collapsible");
+        menuLink.forEach(item => item.removeEventListener("click", closeHamburger));
 				subMenus.forEach(e => {
 					e.addEventListener("mouseenter", openCloseSubMenuOnHover);
 					e.addEventListener("mouseleave", openCloseSubMenuOnHover);
 				});
 				closeAllSubMenuForMobile();
-				subItems.forEach(e => e.classList.remove('flatten'));
+				subItems.forEach(e => e.classList.remove("flatten"));
 				subMenuNames.forEach(e => e.removeEventListener("click", openCloseSubMenuForMobile));
-				chevrons.forEach(e => e.classList.add('hidden', 'no-display'));
+				chevrons.forEach(e => e.classList.add("hidden", "no-display"));
 
     } else {
         hamburger.classList.remove("hidden", "no-display");
         cross.classList.add("hidden", "no-display");
-        menuLink.forEach(item => item.addEventListener('click', closeHamburger));
+        menuLink.forEach(item => item.addEventListener("click", closeHamburger));
         flattenNavItems();
-        setTimeout(() => menu.classList.add('collapsible'), 200);
+        setTimeout(() => menu.classList.add("collapsible"), 200);
 		subMenus.forEach(e => {
 			e.removeEventListener("mouseenter",openCloseSubMenuOnHover);
 			e.removeEventListener("mouseleave", openCloseSubMenuOnHover);
 		});
 		subMenuNames.forEach(e => e.addEventListener("click", openCloseSubMenuForMobile));
-		chevrons.forEach(e => e.classList.remove('hidden', 'no-display'));
+		chevrons.forEach(e => e.classList.remove("hidden", "no-display"));
     }
-}
+};
 
 widthChange(mq);
 mq.addEventListener("change", widthChange);
